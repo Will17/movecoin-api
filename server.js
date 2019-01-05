c828df97bf6800c15fb892226f934b7d9c6e06dc
Glitch (hello-express) <none>
2019-01-05 18:25:14 +0000

    📫😺 Checkpoint ./server.js:2451181/391

--- server.js
+++ server.js
@@ -5,15 +5,11 @@
 const express = require('express');
 const app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get('/', function(req, res) {
+  console.log(req.query)
+  res.sendStatus(200);
+  res.end();
 });
 
 // listen for requests :)
